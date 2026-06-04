255d11309aafe0d3e649d9243e2787d7575d75e3
Monster144 <89910253+MonsterCaptain@users.noreply.github.com>
2026-06-04 18:10:11 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "hormone-v1";
+
+const FILES = [
+  "index.html",
+  "historique.html",
+  "graphiques.html",
+  "profil.html",
+  "app.js",
+  "manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
